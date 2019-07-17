76271dc2d48299ef387a47dd33f8c450bebc414a
Jonathan Bispo <jfb0101@gmail.com>
2019-07-16 21:49:35 -0300

    api sakila completa

--- discover-models.js
+++ discover-models.js
@@ -11,50 +11,62 @@ const DATASOURCE_NAME = 'sakilaDS'
 const dataSourceConfig = require('./server/datasources.json')
 const db = new loopback.DataSource(dataSourceConfig[DATASOURCE_NAME])
 
-const models = ['inventory', 'rental', 'customer','actor',]
+const models = ['inventory', 'rental', 'customer', 'actor', 'address', 'category', 'city', 'country', 'customer', 'film', 'film_actor', 'film_category', 'film_text', 'language', 'payment', 'staff', 'store']
 
+function upperCaseFirstLetter(str) {
+    return str.substring(0,1).toUpperCase() + str.substring(1,str.length)
+}
 async function discover() {
-    let promises = []
+    let currentModel = 0
 
-    models.forEach(model => {
-        promises.push(new Promise(async (resolve, reject) => {
+    function processNextModel() {
+        new Promise(async (resolve,reject) => {
+            let model = models[currentModel]
+
             try {
                 const options = { relations: true }
-
+    
                 const schema = await db.discoverSchemas(model, options)
-
+    
                 await writeFile(
                     `common/models/${model}.json`,
                     JSON.stringify(schema['sakila.' + model], null, 2)
                 )
-
+    
                 const configJson = await readFile('server/model-config.json', 'utf-8')
-
+    
                 const config = JSON.parse(configJson)
 
-                if (config[model] == undefined) {
-                    config[model] = {
+                const modelName = upperCaseFirstLetter(model)
+    
+                if (config[modelName] == undefined) {
+                    config[modelName] = {
                         dataSource: DATASOURCE_NAME,
                         public: true
                     }
-
-
+    
+    
                     await writeFile(
                         'server/model-config.json',
                         JSON.stringify(config, null, 2)
                     )
                 }
-
+    
                 resolve()
             } catch (error) {
                 reject(error)
             }
-        }))
-    })
+        }).then(() => {
+            if (currentModel < models.length) {
+                currentModel++
+                processNextModel()
+            }
+        })
+        
+    }
 
-    return Promise.all(promises)
+    processNextModel()
+
 }
 
-discover().then(() => process.exit()).catch(error =>
-    console.error(`erro ao descobrir modelos ${models.join()}`, error)
-)
+discover()
